bd39fa3e5fe20b0fdaab5facfd97b8f30fc4a655
alfinanur17 <alfinanur0117@gmail.com>
2021-09-06 20:03:28 +0700

    mengganti nama

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Alfina Nur Azizah
+                                    Alfina Nur Azizah MI 2E
                                 </div>
                             </div>
                         </div>
